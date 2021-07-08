c95a549fecdeb57cb437386f6b1f99e01e98b5bc
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-07-09 11:22:20 +1200

    Working on show all | Show All Part 2 (video) 0:00

--- 91902_Support_Files/L3_Site_Template/index.php
+++ 91902_Support_Files/L3_Site_Template/index.php
@@ -1,90 +1,51 @@
 <!DOCTYPE HTML>
 
+<?php
+
+session_start();
+include("config.php")
+include("function.php");
+
+// Connect to database...
+$dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+if(mysqli_connect_errno()) {
+    echo "Connection failed:".mysqli_connect_error();
+    exit;
+}
+
+?>
+
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Put Content Here">
-    <meta name="keywords" content="Put keywords here">
-    <meta name="author" content="Put your name here">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>DB Name</title>
-    
-    <!-- Edit the link below / replace with your chosen google font -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
-    
-    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="theme/quick_quotes.css.css"> 
-        <link rel="stylesheet" href="theme/font-awesome.min.css">
-    
-</head>
+<?php include("content/head.php"); ?>
     
 <body>
     
     <div class="wrapper">
     
-
-        
-        <div class="box banner">
-            
-    
-            <h1>DB Name</h1>
-        </div>    <!-- / banner -->
-
-        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
-        <div class="box nav">
-            
-            <div class="linkwrapper">
-                <div class="commonsearches">
-                    <a href="#l">Item</a> | 
-                    <a href="#">Item</a> | 
-                    <a href="#">Item</a> 
-                </div>  <!-- / common searches -->
-            
-                <div class="topsearch">
-                    
-                    <!-- Quick Search -->           
-                    <form method="post" action="quick_search.php" enctype="multipart/form-data">
-
-                        <input class="search quicksearch" type="text" name="quick_search" size="40" value="" required placeholder="Quick Search..." />
-
-                        <input class="submit" type="submit" name="find_quick" value="&#xf002;" />
-
-                    </form>     <!-- / quick search -->
-                    
-                </div>  <!-- / top search -->
-                
-                <div class="topadmin">
-                    <a href="#">Log In</a>
-                    
-                </div>  <!-- / top admin -->
-                
-            </div>  <!--- / link wrapper -->
-            
-        </div>    <!-- / nav -->        
+            <?php include("content/heading_navigation.php") ?>
         
         <div class="box main">
-            <h2>First Page heading</h2>
             
-            <p>
-                Sugar plum powder dessert bonbon powder lollipop lollipop muffin I love. Tiramisu dessert I love sugar plum apple pie marzipan chocolate bar brownie. Pudding tiramisu caramels biscuit tiramisu. I love gummies gummies pie sweet pudding I love I love. Donut halvah candy oat cake donut. Caramels wafer danish jelly-o.
-            </p>
-            
-            <p>
-                Jelly beans gingerbread jelly tootsie roll. Lemon drops bear claw I love tiramisu candy canes cake. Jelly-o apple pie powder tart I    love candy tiramisu. Topping liquorice cake sesame snaps brownie. Lemon drops jelly beans brownie danish I love fruitcake cotton candy danish. Dragée pastry halvah macaroon I love chocolate bar caramels liquorice. Halvah cupcake muffin fruitcake pastry I love bear claw I love jelly. Tart chocolate bar croissant danish jelly. Sugar plum powder cupcake muffin gingerbread I love sweet.
-            </p>
-            
-            <p>
-                Biscuit jelly I love jelly-o candy canes chupa chups topping lemon drops I love. Sesame snaps macaroon lemon drops carrot cake I love. Chocolate bar gingerbread chupa chups. Dessert chocolate pastry liquorice tootsie roll lemon drops sweet macaroon liquorice. I love muffin bonbon. Cupcake apple pie chocolate cake bonbon topping. Macaroon gummi bears dragée caramels donut chocolate.
-            </p>
+           <?php
+    
+            if(!isset($_REQUEST['page'])) {
+                include("content/home.php");
+            }
+                // end of if that includes home page
+            else {
+                // prevents users from navigating through file system
+                $page=preg_replace('/[^G-9a-zA-Z]-/','',$_REQUEST['page']);
+                include("content/$page.php");
+                
+            } // end of else that includes requested content
+                
+            ?>
             
         </div>    <!-- / main -->
         
-
-        <div class="box footer">
-            CC yourname 20XX
-        </div>    <!-- / footer -->
+        <?php include("content/footer.php"); ?>
     
     </div>  <!-- / wrapper  -->
     
