07f43ec4035d588dc946fed52b4418d323e8b57b
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-07-27 15:09:45 +1200

    working on Author Pages Part 1 (video)

--- 91902_Support_Files/L3_Site_Template/content/author.php
+++ 91902_Support_Files/L3_Site_Template/content/author.php
@@ -1,12 +1,22 @@
-<h2>All Results</h2>
-
 <?php
+
+if(!isset($_REQUEST['authorID']))
+{
+    header('Location: index.php');
+}
+
+$author_to_find = $_REQUEST['authorID'];
 
 $find_sql = "SELECT * FROM `quotes`
 JOIN author ON (`author`.`Author_ID` = `quotes`.`Author_ID`)
 ";
 $find_query = mysqli_query($dbconnect, $find_sql);
 $find_rs = mysqli_fetch_assoc($find_query);
+
+?>
+
+<h2>All Results</h2>
+<?php
 
 // Loop through results and display them...
 do {
