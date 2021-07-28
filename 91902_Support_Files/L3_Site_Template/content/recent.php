ab82a0c868a35d29adfb7b9bcee2568ad75f2b72
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-07-28 14:09:18 +1200

    Working on Random / Recent Quotes (video)

---
+++ 91902_Support_Files/L3_Site_Template/content/recent.php
@@ -0,0 +1,44 @@
+<h2>All Results</h2>
+
+<?php
+
+$find_sql = "SELECT * FROM `quotes`
+JOIN author ON (`author`.`Author_ID` = `quotes`.`Author_ID`)
+";
+$find_query = mysqli_query($dbconnect, $find_sql);
+$find_rs = mysqli_fetch_assoc($find_query);
+
+// Loop through results and display them...
+do {
+    
+    $quote = preg_replace('/[^A-Za-z0-9.,\s\'\-]/', ' ', $find_rs['Quote']);
+    
+    // get author name
+    include("get_author.php");
+    
+    ?>
+    <div class="results">
+    <p>
+        <?php echo $quote; ?><br />
+        
+        <!-- display author name -->
+        <a href="index.php?page=author&authorID=<?php echo 
+        $find_rs['Author_ID']; ?>">
+            <?php echo $full_name; ?>
+        </a>
+        
+    </p>
+    
+    <?php include("show_subjects.php"); ?>
+   
+</div>
+
+<br />
+
+<?php
+    
+}   // end of display results 'do'
+    
+while($find_rs = mysqli_fetch_assoc($find_query))
+
+?>
