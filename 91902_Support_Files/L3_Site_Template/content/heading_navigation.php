d15885a8cbf13bd004a9a282033ca0c8aa71391a
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-08-03 14:47:46 +1200

    Log In Part 1 (video) complete

--- 91902_Support_Files/L3_Site_Template/content/heading_navigation.php
+++ 91902_Support_Files/L3_Site_Template/content/heading_navigation.php
@@ -31,7 +31,29 @@
                 </div>  <!-- / top search -->
                 
                 <div class="topadmin">
-                    <a href="#">Log In</a>
+                    
+                    <?php
+                    
+                    if(isset($_SESSION['admin'])) {
+                        
+                    ?>
+                    
+                    Add Quote &nbsp; &nbsp;
+                    Log out
+                    
+                    <?php
+            
+                    } // end user is logged in if 
+                    
+                    else {
+                        ?>
+                    
+                    <a href="index.php?page../admin/login">Login</a>
+                    <?php
+                    }   // end of login else
+                    
+                    ?>
+                    
                     
                 </div>  <!-- / top admin -->
                 
