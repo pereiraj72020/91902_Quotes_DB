e6a945bf2b0a3b703e8b9625856f135aab35ce5a
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-08-13 13:43:53 +1200

    Changed Initial to Middle

--- 91902_Support_Files/L3_Site_Template/content/get_author.php
+++ 91902_Support_Files/L3_Site_Template/content/get_author.php
@@ -2,7 +2,7 @@
 
     // author name...
     $first = $find_rs['First'];
-    $middle = $find_rs['Initial'];
+    $middle = $find_rs['Middle'];
     $last = $find_rs['Last'];
     
     $full_name = $first." ".$middle." ".$last;
