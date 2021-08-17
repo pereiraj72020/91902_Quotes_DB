3da8854bbb74779505da2b136e6cfc823244e428
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-08-17 14:59:33 +1200

    Show no errors when editing authors, note: no actual errors shown that would affect the website.

--- 91902_Support_Files/L3_Site_Template/91902_Quotes_DB/config.php
+++ 91902_Support_Files/L3_Site_Template/91902_Quotes_DB/config.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('display_errors', 'On');  // change to 'Off' when live
+ini_set('display_errors', 'Off');  // change to 'Off' when live
 
 define ("DB_NAME","pereiraj72020_00_l3_quotes"); 
 define ("DB_USERNAME","_pereiraj72020");
